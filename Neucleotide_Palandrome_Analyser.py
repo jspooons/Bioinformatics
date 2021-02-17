c3e76754fa7aa1cf238b690af787b2f9db6e21f8
Jordan Spooner <jordanspooner05@gmail.com>
2021-02-17 16:18:11 +0000

    Red text and index applied to letters where a nucleotide palandrome exists

--- Neucleotide_Palandrome_Analyser.py
+++ Neucleotide_Palandrome_Analyser.py
@@ -14,8 +14,6 @@ ORANGE = (255, 165, 0)
 LIGHTGREY = (220, 220, 220)
 
 
-
-
 def equal(str, arr):
     for i in range(len(str) - 1):
         if str[i] == arr[i]:
@@ -33,7 +31,7 @@ def getSequenceAndMinPalandromeLength():
     #sequence = input("Enter your sequence: \n")
     #print('\n')
     #sequence = 'AAATTTATAGAAGAGTCTAGACATG'
-    sequence = 'GAAATTTAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAATTTAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAG'
+    sequence = 'GAAATTTAAAAAAAAAATTTAAAATTTG'
     sequence.replace(" ", "")
     sequence.upper()
 
@@ -64,8 +62,8 @@ def findPalandromes(minPal, sequence, comp_seq):
         l = minPal
         while l + j <= len_seq:
             if equal(sequence[j:l + j], list(reversed(comp_seq[j:l + j]))):
-                palandromes_and_pos.append([sequence[j:l + j], j + 1]) # palandrome and position
-                palandrome_data.append([j+1, l]) # position of a palandrome and the length of it
+                palandromes_and_pos.append([sequence[j:l + j], j + 1])  # palandrome and position
+                palandrome_data.append([j+1, l])  # position of a palandrome and the length of it
                 # palandromes.append(sequence[j:l+j])
             l += 1
 
@@ -73,69 +71,6 @@ def findPalandromes(minPal, sequence, comp_seq):
     print(palandromes_and_pos)
 
     return palandrome_data
-
-
-def renderTextCenteredAt(text, font, colour, x, y, d, allowed_width, info, img):
-    # first, split the text into words
-    words = list(text)
-
-    # now, construct lines out of these words
-    lines = []
-    lines_ = []
-    counter = 0
-    # TO DO: counter for counting letter position
-    while len(words) > 0:
-        # get as many words as will fit within allowed_width
-        line_words = []
-        while len(words) > 0:
-            counter+=1
-            #print(counter)
-            for i in range(len(info)):
-                if (info[i][0] == counter):
-                    line_words[len(line_words)-1] = line_words[len(line_words)-1]
-
-            line_words.append(words.pop(0))
-            #print(line_words)
-            #if (len(line_words) == 0):
-
-
-            fw, fh = font.getsize(' '.join(line_words + words[:1]))
-            if fw > allowed_width:
-                break
-
-        # add a line consisting of those words
-        line = ' '.join(line_words)
-        lines.append(line)
-    #print(lines)
-    #lines[0][0] = lines[0][0]+'\u0332'
-    #test = [['d','d']]
-    #test[0][0] = test[0][0]+'e'+'d'
-    #print(test)
-
-    # now we've split our text into lines that fit into the width, actually
-    # render them
-
-    # we'll render each line below the last, so we need to keep track of
-    # the culmative height of the lines we've rendered so far
-    y_offset = 0
-    ty=0
-    for line in lines:
-        line_len = len(line)
-
-
-
-        fw, fh = font.getsize(line)
-
-        # (tx, ty) is the top-left of the font surface
-        tx = x - fw / 2
-        ty = y + y_offset
-        #font.set_underline(True)
-
-        d.text((tx, ty), line, font=font, fill=BLACK)
-
-        y_offset += fh
-
-    return img, ty+fh+5
 
 
 def findLargestChar(chars, font):
@@ -152,42 +87,87 @@ def checkFit(width, height, text, font, largest):
     largest_size = font.getsize(largest)
     length = len(text)
 
-    width_amount = floor(width/largest_size)
-    height_amount = floor(height/largest_size)
+    width_amount = floor(width/largest_size[0])
+    height_amount = floor(height/largest_size[1])
 
-    if width_amount * height_amount <= length:
-        return False
+    if length > ((floor(width_amount/2) + 1) * height_amount):  # too big (letters over fill)
+        return 0, -1, -1
+    elif length < ((floor(width_amount/2) + 1) * (height_amount-1)) + 1:  # too small (letters under fill)
+        return 1, -1, -1
+    else:
+        return 2, width_amount, height_amount
 
-    return True, width_amount, height_amount
 
-
-def optimise(no_chars):
+# tries to optimise for a square grid of letters
+def optimise(num_elems):
     cols = 1;
     rows = 1;
 
-    while (no_chars > cols * rows):
+    while (num_elems > cols * rows):
         cols += 1;
-        if (no_chars > cols * rows):
+        if (num_elems > cols * rows):
             rows += 1;
         else:
             break
 
+    return cols, rows
+
+
+def column(matrix, i):
+    return [row[i]-1 for row in matrix]
+
 
 def drawText(sequence, seq_info, width, height, colour, background):
     img = Image.new('RGB', (width, height), color=background)
-
-    fnt = ImageFont.truetype('/Library/Fonts/arial.ttf', 20)
     d = ImageDraw.Draw(img)
+    fit = -1
+    fontsize = 8
+    allowed_num_cols = 0
+    allowed_num_rows = 0
+    w = 0
+    h = 0
+    fnt = ''
+    pal_positions = column(seq_info,0)
 
-    img2 = img.crop((0,0,width,height))
-    img2.save('pil_text_font.png')
-    img2.show()
+    while fit != 2:
+        fnt = ImageFont.truetype('/Library/Fonts/arial.ttf', fontsize)
+        largest = findLargestChar(['A', 'T', 'C', 'G'], fnt)
+        w, h = fnt.getsize(largest)
+
+        fit, allowed_num_cols, allowed_num_rows = checkFit(width, height, sequence, fnt, largest)
+
+        if fit == 0:
+            fontsize -= 1
+        elif fit == 1:
+            fontsize += 1
+
+    counter = 0
+    for i in range(allowed_num_rows):
+        for j in range(allowed_num_cols):
+            if j % 2 == 0:
+                if (counter== len(sequence)):
+                    break
+
+                if counter in pal_positions:
+                    d.text((j * w, i * h), sequence[counter], font=fnt, fill=RED)
+                    subfontsize = floor(fontsize/4)
+                    subfnt = ImageFont.truetype('/Library/Fonts/arial.ttf', subfontsize)
+                    d.text((j * w +w, i * h +h*0.8), str(counter+1), font=subfnt, fill=BLUE)
+                else:
+                    d.text((j * w, i * h), sequence[counter], font=fnt, fill=colour)
+
+                counter += 1
+
+    img.save('pil_text_font.png')
+    img.show()
 
 
 def main():
     sequence, minPal = getSequenceAndMinPalandromeLength()
     comp_seq = getReverseComplement(sequence)
     palandrome_data = findPalandromes(minPal, sequence, comp_seq)
+
+    drawText(sequence, palandrome_data, 900, 600, BLACK, LIGHTGREY)
 
 
 if __name__ == "__main__":
